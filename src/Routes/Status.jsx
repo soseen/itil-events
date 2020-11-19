e3248c84f81529b50e4196f3407b19d0e87a63b5
soseen <lampard082@gmail.com>
2020-11-19 12:14:35 +0100

    status edit

--- src/Routes/Status.jsx
+++ src/Routes/Status.jsx
@@ -3,8 +3,17 @@ import './Status.css'
 
 const Status = () => {
     return(
-        <div className='page-container'>
-            <h1 className='page-title'>Status</h1>
+        <div className='status-page-container'>
+            {/* <h1 className='page-title'>Status</h1> */}
+            
+            <div className="dashboard-container">
+                <div className='services-container'>
+
+                </div>
+                <div className='stats-container'>
+
+                </div>
+            </div>
         </div>
     )
 }
