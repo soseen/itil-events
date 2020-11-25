27e320b626117d66dfb1cb4363c996b651113b4f
soseen <lampard082@gmail.com>
2020-11-25 23:14:52 +0100

    Status component development

--- src/Routes/Status.jsx
+++ src/Routes/Status.jsx
@@ -5,13 +5,28 @@ const Status = () => {
     return(
         <div className='status-page-container'>
             {/* <h1 className='page-title'>Status</h1> */}
-            
-            <div className="dashboard-container">
-                <div className='services-container'>
-
+        
+            <div className='status-content-container'>
+                <div className='status-header-container'>
+                    <div className='status-header'>
+                        <p>Status</p>
+                    </div>
                 </div>
-                <div className='stats-container'>
+                <div className='status-content'>
+                    <div className='dashboard-container'>
+                        <div className='dashboard'>
+                            <div className='services-container'>
+                                <p>Service</p>
+                            </div>
+                            <div className='service-events-container'>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='stats-container'>
+                        <div className='stats'>
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
