999383739cba9d49507f1540f76d031d1d74d535
soseen <lampard082@gmail.com>
2021-04-24 04:37:43 +0200

    netlify deploy

---
+++ src/components/SideNavData.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { AiFillHome, AiFillWarning, AiFillAlert, AiOutlineLogout } from "react-icons/ai";
+import {BsFillPersonFill} from "react-icons/bs";
+
+export const SideNavData = [
+    {
+        name: 'Status',
+        route: '/',
+        icon: <AiFillHome />
+    },
+    {
+        name: 'Alerts',
+        route: '/alerts',
+        icon: <AiFillAlert />
+    },
+    {
+        name: 'Rules',
+        route: '/rules',
+        icon: <AiFillWarning />
+    },
+    {
+        name: 'Tasks',
+        route: '/tasks',
+        icon: <BsFillPersonFill />
+    },
+    {
+        name: 'Log Out',
+        route: '/',
+        icon: <AiOutlineLogout />
+    }
+];
